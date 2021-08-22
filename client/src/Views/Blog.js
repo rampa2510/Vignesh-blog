14213a3f4539db2bff89af119442955d7ccdf3a3
rampa2510 <iamram2510@gmail.com>
2021-08-22 10:52:22 +0530

    Removed log statements

--- client/src/Views/Blog.js
+++ client/src/Views/Blog.js
@@ -9,7 +9,6 @@ export default function BlogView({ data }) {
   const [color, setColor] = useState(colorMode);
 
   useEffect(() => {
-    console.log(colorMode);
     setColor(colorMode);
   }, [colorMode]);
 
--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import { ColorModeScript } from "@chakra-ui/react";
 
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+// import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -18,4 +18,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
+// reportWebVitals(console.log);
